9d26fde691798956e12f4db76e0ddcc077695d9c
Mark Lavercombe <mlava@users.noreply.github.com>
2022-09-23 07:29:20 +1000

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,3 +1,116 @@
+// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/components/FormDialog.tsx
+const FormDialog = ({
+    onSubmit,
+    title,
+    options,
+    question,
+    onClose,
+}) => {
+    const [data, setData] = window.React.useState(options[0].id);
+    const onClick = window.React.useCallback(
+        () => {
+            onSubmit(data);
+            onClose();
+        },
+        [data, onClose]
+    );
+    const onCancel = window.React.useCallback(
+        () => {
+            onSubmit("");
+            onClose();
+        },
+        [onClose]
+    )
+    return window.React.createElement(
+        window.Blueprint.Core.Dialog,
+        { isOpen: true, onClose: onCancel, title, },
+        window.React.createElement(
+            "div",
+            { className: window.Blueprint.Core.Classes.DIALOG_BODY },
+            question,
+            window.React.createElement(
+                window.Blueprint.Core.Label,
+                {},
+                "Movies:",
+                window.React.createElement(
+                    window.Blueprint.Select.Select,
+                    {
+                        activeItem: data,
+                        onItemSelect: (id) => setData(id),
+                        items: options.map(opt => opt.id),
+                        itemRenderer: (item, { modifiers, handleClick }) => window.React.createElement(
+                            window.Blueprint.Core.MenuItem,
+                            {
+                                key: item,
+                                text: options.find(opt => opt.id === item).label,
+                                active: modifiers.active,
+                                onClick: handleClick,
+                            }
+                        ),
+                        filterable: false,
+                        popoverProps: {
+                            minimal: true,
+                            captureDismiss: true,
+                        }
+                    },
+                    window.React.createElement(
+                        window.Blueprint.Core.Button,
+                        {
+                            text: options.find(opt => opt.id === data).label,
+                            rightIcon: "double-caret-vertical"
+                        }
+                    )
+                )
+            )
+        ),
+        window.React.createElement(
+            "div",
+            { className: window.Blueprint.Core.Classes.DIALOG_FOOTER },
+            window.React.createElement(
+                "div",
+                { className: window.Blueprint.Core.Classes.DIALOG_FOOTER_ACTIONS },
+                window.React.createElement(
+                    window.Blueprint.Core.Button,
+                    { text: "Cancel", onClick: onCancel, }
+                ),
+                window.React.createElement(
+                    window.Blueprint.Core.Button,
+                    { text: "Submit", intent: "primary", onClick }
+                )
+            )
+        )
+    );
+}
+
+const prompt = ({
+    options,
+    question,
+    title,
+}) =>
+    new Promise((resolve) => {
+        const app = document.getElementById("app");
+        const parent = document.createElement("div");
+        parent.id = 'imdb-prompt-root';
+        app.parentElement.appendChild(parent);
+
+        window.ReactDOM.render(
+            window.React.createElement(
+                FormDialog,
+                {
+                    onSubmit: resolve,
+                    title,
+                    options,
+                    question,
+                    onClose: () => {
+                        window.ReactDOM.unmountComponentAtNode(parent);
+                        parent.remove();
+                    }
+                }
+            ),
+            parent
+        )
+    });
+
 const config = {
     tabTitle: "Streaming Availability",
     settings: [
@@ -27,19 +140,26 @@ export default {
         extensionAPI.settings.panel.create(config);
 
         window.roamAlphaAPI.ui.commandPalette.addCommand({
-            label: "Get Streaming Availability",
-            callback: () => getAvailability({ extensionAPI })
+            label: "Get Streaming Availability (Current block)",
+            callback: () => getAvailability({ extensionAPI }, true)
         });
 
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Get Streaming Availability (All Child blocks)",
+            callback: () => getAvailability({ extensionAPI }, false)
+        });
     },
     onunload: () => {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
-            label: 'Get Streaming Availability'
+            label: 'Get Streaming Availability (Current block)'
+        });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Get Streaming Availability (All Child blocks)'
         });
     }
 }
 
-async function getAvailability({ extensionAPI }) {
+async function getAvailability({ extensionAPI }, parentOnly) {
     var rAPIkey, rAPIcc, omdbKey, key;
     var searchBlock = undefined;
     breakme: {
@@ -57,42 +177,208 @@ async function getAvailability({ extensionAPI }) {
             rAPIcc = extensionAPI.settings.get("sa-country");
         }
 
-        searchBlock = await window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
-        if (searchBlock == undefined) {
-            key = "focus";
-            sendConfigAlert(key);
-            break breakme;
-        }
-        let q = `[:find (pull ?page [:block/string]) :where [?page :block/uid "${searchBlock}"]  ]`;
-        var searchBlockInfo = await window.roamAlphaAPI.q(q);
-        var searchString = searchBlockInfo[0][0].string;
-
-        var url = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&t=" + searchString + "";
-        var imdbID = await fetch(url).then((response) => response.json()).then((data) => { return data.imdbID });
-
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': rAPIkey,
-                'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+        if (parentOnly) { // get data for one film only
+            searchBlock = await window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+            if (searchBlock == undefined) {
+                key = "focus";
+                sendConfigAlert(key);
+                break breakme;
             }
-        };
-        await fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=' + rAPIcc + '&imdb_id=' + imdbID, options)
-            .then(response => response.json())
-            .then(response => {
-                if (Object.keys(response.streamingInfo).length > 0) {
-                    for (var i = 0; i < Object.keys(response.streamingInfo).length; i++) {
-                        var obj = response.streamingInfo;
-                        var name = Object.keys(obj)[i];
-                        var link = Object.entries(obj[name][rAPIcc])[0][1];
-                        var linkString = "[" + name[0].toUpperCase() + name.substring(1) + "](" + link + ")";
-                        createLink(searchBlock, linkString)
-                    }
+            let q = `[:find (pull ?page [:block/string :block/uid {:block/children ...}]) :where [?page :block/uid "${searchBlock}"]  ]`;
+            var searchBlockInfo = await window.roamAlphaAPI.q(q);
+            var searchString = searchBlockInfo[0][0].string;
+            var finalSearchString = searchString.split("(");
+            if (finalSearchString != null) {
+                searchString = finalSearchString[0];
+            }
+
+            var url = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&t=" + searchString + "";
+            var url1 = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&s=" + searchString + "";
+            var imdbID = await fetch(url).then((response) => response.json()).then(async (data) => {
+                if (data.hasOwnProperty("Response") && data.Response == "False") {
+                    return null;
                 } else {
-                    alert("No streaming services available!");
+                    var newString = "**" + data.Title + "** (" + data.Year + ")";
+                    window.roamAlphaAPI.updateBlock({ block: { uid: searchBlock, string: newString, open: true } });
+                    var ratingsString = "";
+                    if (data.hasOwnProperty("Ratings") && data.Ratings.length > 0) {
+                        for (var i = 0; i < data.Ratings.length; i++) {
+                            ratingsString += "" + data.Ratings[i].Source + ": " + data.Ratings[i].Value + "";
+                            if (i < data.Ratings.length - 1) {
+                                ratingsString += "\n";
+                            }
+                        }
+                        createLink(searchBlock, ratingsString, 1);
+                    }
+                    return data.imdbID;
                 }
-            })
-            .catch(err => console.error(err));
+            });
+            if (imdbID == null) {
+                imdbID = await fetch(url1).then(r => r.json()).then((movies) => {
+                    const options = movies.Search
+                        .filter(m => m.Type === "movie" || m.Type === "series")
+                        .map(m => ({ label: "" + m.Title + " (" + m.Year + ")", id: m.imdbID }));
+                    return prompt({
+                        title: "IMDB",
+                        question: "Which movie do you mean?",
+                        options,
+                    });
+                })
+                var url = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&i=" + imdbID + "&plot=full";
+                await fetch(url).then((response) => response.json()).then(async (data) => {
+                    var newString = "**" + data.Title + "** (" + data.Year + ")";
+                    window.roamAlphaAPI.updateBlock({ block: { uid: searchBlock, string: newString, open: true } });
+                    var ratingsString = "";
+                    if (data.hasOwnProperty("Ratings") && data.Ratings.length > 0) {
+                        for (var i = 0; i < data.Ratings.length; i++) {
+                            ratingsString += "" + data.Ratings[i].Source + ": " + data.Ratings[i].Value + "";
+                            if (i < data.Ratings.length - 1) {
+                                ratingsString += "\n";
+                            }
+                        }
+                        createLink(searchBlock, ratingsString, 1);
+                    }
+                })
+            };
+
+            if (searchBlockInfo[0][0].hasOwnProperty("children")) {
+                for (var i = 0; i < searchBlockInfo[0][0].children.length; i++) {
+                    await window.roamAlphaAPI.deleteBlock({ "block": { "uid": searchBlockInfo[0][0].children[i].uid } })
+                }
+            }
+            const options = {
+                method: 'GET',
+                headers: {
+                    'X-RapidAPI-Key': rAPIkey,
+                    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+                }
+            };
+            await fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=' + rAPIcc + '&imdb_id=' + imdbID, options)
+                .then(response => response.json())
+                .then(response => {
+                    console.info(response);
+                    if (Object.keys(response.streamingInfo).length > 0) {
+                        for (var i = 0; i < Object.keys(response.streamingInfo).length; i++) {
+                            var obj = response.streamingInfo;
+                            var name = Object.keys(obj)[i];
+                            var link = Object.entries(obj[name][rAPIcc])[0][1];
+                            var linkString = "[" + name[0].toUpperCase() + name.substring(1) + "](" + link + ")";
+                            createLink(searchBlock, linkString, i + 1);
+                        }
+                    } else {
+                        alert("No streaming services available!");
+                    }
+                })
+                .catch(err => console.error(err));
+        } else {  // search for all films under heading
+            var parentBlock = await window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+            if (parentBlock == undefined) {
+                key = "focus";
+                sendConfigAlert(key);
+                break breakme;
+            }
+            let q = `[:find (pull ?page [:block/string :block/uid {:block/children ...}]) :where [?page :block/uid "${parentBlock}"]  ]`;
+            var parentBlockInfo = await window.roamAlphaAPI.q(q);
+            if (parentBlockInfo[0][0].hasOwnProperty("children")) {
+                for (var i = 0; i < parentBlockInfo[0][0].children.length; i++) {
+                    if (parentBlockInfo[0][0].children[i].hasOwnProperty("children")) {
+                        for (var j = 0; j < parentBlockInfo[0][0].children[i].children.length; j++) {
+                            await window.roamAlphaAPI.deleteBlock({ "block": { "uid": parentBlockInfo[0][0].children[i].children[j].uid } })
+                        }
+                    }
+                }
+            }
+            parentBlockInfo = await window.roamAlphaAPI.q(q);
+            
+            if (parentBlockInfo[0][0].hasOwnProperty("children")) {
+                for (var i = 0; i < parentBlockInfo[0][0].children.length; i++) {
+                    var searchString = parentBlockInfo[0][0].children[i].string;
+                    var finalSearchString = searchString.split("(");
+                    if (finalSearchString != null) {
+                        searchString = finalSearchString[0].trim();
+                        searchString = searchString.replaceAll('**','');
+                    }
+                    searchBlock = parentBlockInfo[0][0].children[i].uid;
+                    let q = `[:find (pull ?page [:block/string :block/uid {:block/children ...}]) :where [?page :block/uid "${searchBlock}"]  ]`;
+                    var searchBlockInfo = await window.roamAlphaAPI.q(q);
+                    
+                    var url = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&t=" + searchString + "";
+                    var url1 = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&s=" + searchString + "";
+                    var imdbID = await fetch(url).then((response) => response.json()).then(async (data) => {
+                        if (data.hasOwnProperty("Response") && data.Response == "False") {
+                            return null;
+                        } else {
+                            var newString = "**" + data.Title + "** (" + data.Year + ")";
+                            window.roamAlphaAPI.updateBlock({ block: { uid: searchBlock, string: newString, open: true } });
+                            var ratingsString = "";
+                            if (data.hasOwnProperty("Ratings") && data.Ratings.length > 0) {
+                                for (var i = 0; i < data.Ratings.length; i++) {
+                                    ratingsString += "" + data.Ratings[i].Source + ": " + data.Ratings[i].Value + "";
+                                    if (i < data.Ratings.length - 1) {
+                                        ratingsString += "\n";
+                                    }
+                                }
+                                createLink(searchBlock, ratingsString, 1);
+                            }
+                            return data.imdbID;
+                        }
+                    });
+                    if (imdbID == null) {
+                        imdbID = await fetch(url1).then(r => r.json()).then((movies) => {
+                            const options = movies.Search
+                                .filter(m => m.Type === "movie" || m.Type === "series")
+                                .map(m => ({ label: "" + m.Title + " (" + m.Year + ")", id: m.imdbID }));
+                            return prompt({
+                                title: "IMDB",
+                                question: "Which movie do you mean?",
+                                options,
+                            });
+                        })
+                        var url = "https://www.omdbapi.com/?apiKey=" + omdbKey + "&i=" + imdbID + "&plot=full";
+                        await fetch(url).then((response) => response.json()).then(async (data) => {
+                            var newString = "**" + data.Title + "** (" + data.Year + ")";
+                            window.roamAlphaAPI.updateBlock({ block: { uid: searchBlock, string: newString, open: true } });
+                            var ratingsString = "";
+                            if (data.hasOwnProperty("Ratings") && data.Ratings.length > 0) {
+                                for (var i = 0; i < data.Ratings.length; i++) {
+                                    ratingsString += "" + data.Ratings[i].Source + ": " + data.Ratings[i].Value + "";
+                                    if (i < data.Ratings.length - 1) {
+                                        ratingsString += "\n";
+                                    }
+                                }
+                                createLink(searchBlock, ratingsString, 1);
+                            }
+                        })
+                    };
+
+                    const options = {
+                        method: 'GET',
+                        headers: {
+                            'X-RapidAPI-Key': rAPIkey,
+                            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+                        }
+                    };
+                    await fetch('https://streaming-availability.p.rapidapi.com/get/basic?country=' + rAPIcc + '&imdb_id=' + imdbID, options)
+                        .then(response => response.json())
+                        .then(response => {
+                            ;
+                            if (Object.keys(response.streamingInfo).length > 0) {
+                                for (var i = 0; i < Object.keys(response.streamingInfo).length; i++) {
+                                    var obj = response.streamingInfo;
+                                    var name = Object.keys(obj)[i];
+                                    var link = Object.entries(obj[name][rAPIcc])[0][1];
+                                    var linkString = "[" + name[0].toUpperCase() + name.substring(1) + "](" + link + ")";
+                                    createLink(searchBlock, linkString, i + 1);
+                                }
+                            } else {
+                                var linkString = "No streaming services available for this title";
+                                createLink(searchBlock, linkString, i + 1);
+                            }
+                        })
+                        .catch(err => console.error(err));
+                }
+            }
+        }
     }
 }
 
@@ -106,10 +392,10 @@ function sendConfigAlert(key) {
     }
 }
 
-async function createLink(searchBlock, linkString) {
+async function createLink(searchBlock, linkString, order) {
     var childBlock = window.roamAlphaAPI.util.generateUID();
     window.roamAlphaAPI.createBlock({
-        location: { "parent-uid": searchBlock, order: 1 },
+        location: { "parent-uid": searchBlock, order: order },
         block: { string: linkString, uid: childBlock }
     });
 }
